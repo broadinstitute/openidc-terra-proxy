c8c335485f03765a24920c09713e8ffe6d7dbfc7
Robert Title <rtitle@broadinstitute.org>
2022-03-01 10:17:57 -0500

    Disable Expect: 100-continue in the php introspection endpoint

--- introspect.php
+++ introspect.php
@@ -21,7 +21,9 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => 'access_token='.$access_token.'&token_type_hint=access_token',
   CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/x-www-form-urlencoded'
+    'Content-Type: application/x-www-form-urlencoded',
+    // Disable Expect: 100-continue since it's not supported by the google tokeninfo endpoint.
+    'Expect:'
   ),
 ));
 
