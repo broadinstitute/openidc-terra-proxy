ac35abdaf24b7abff6d9f66e6c4b108c862e8447
Robert Title <rtitle@broadinstitute.org>
2022-03-07 13:24:31 -0500

    Add image based on RedHat ubi-minimal (#7)
    * Add image based on RedHat ubi-minimal
    
    * Expose some ports
    
    * Add listen on ssl port
    
    * Add php-curl
    
    * Add php-json
    
    * Don't replace the ca-bundle crt
    
    * Fix cert directories
    
    * Don't blow away existing ca-bundle file
    
    * verify.exp=required

---
+++ setup-httpd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+echo "Setting up http environment"
+
+ln -s /etc/pki/tls/private /etc/ssl/private
+
+# Generate a snakeoil certificate in case it's needed
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/C=US/ST=Massachusetts/L=Cambridge/O=Random Bits/OU=Widgets/CN=localhost/emailAddress=webmaster@example.org" \
+    -keyout /etc/ssl/private/server.key \
+    -out /etc/ssl/certs/server.crt
+
+# Use the snakeoil cert as the ca-bundle.crt as well
+cp /etc/ssl/certs/server.crt /etc/ssl/certs/server-ca-bundle.crt
+
+# Remove default ssl.conf
+rm /etc/httpd/conf.d/ssl.conf
