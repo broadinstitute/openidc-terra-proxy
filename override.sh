efa5452327dee0aa98feeb92a6c0d2c89bcaaabd
Jack Warren <jack@jackwarren.info>
2023-05-26 14:40:16 -0400

    add env

--- override.sh
+++ override.sh
@@ -188,4 +188,8 @@ if [ -z "$ALLOW_EXPRESSION" ] ; then
     export ALLOW_EXPRESSION='true'
 fi
 
+# default gaceful shutdown timeout
+if [ -z "$GRACEFUL_SHUTDOWN_TIMEOUT"]; then
+    export GRACEFUL_SHUTDOWN_TIMEOUT=15
+fi
 
